f11f030448f18dc90d926fb89115b8ec16ffb046
Swejal-web <swejalshrestha08@gmail.com>
2022-02-04 18:07:36 +0545

    react-leaflet

--- src/App.js
+++ src/App.js
@@ -1,43 +1,77 @@
-import React from "react";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
-import * as parkData from "./data/skateboard-parks.json";
-import "./app.css";
+import React, { useEffect } from 'react';
+
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import * as parkData from './data/skateboard-parks.json';
+import * as schools from './data/export.json';
+import './App.css';
 
 export const icon = new Icon({
-  iconUrl: "/skateboarding.svg",
-  iconSize: [25, 25]
+  iconUrl: '/skateboarding.svg',
+  iconSize: [25, 25],
 });
 
 export default function App() {
   const [activePark, setActivePark] = React.useState(null);
 
+  console.log(schools);
+
+  let Polygon = schools.features.filter(
+    (school) => school.geometry.type === 'Polygon'
+  );
+  let Point = schools.features.filter(
+    (school) => school.geometry.type === 'Point'
+  );
+
+  //let array = schools.elements.filter((school) => school.type === 'node');
+
+  // for (let i in schools.elements) {
+  //   if (schools.elements[i].type === 'node') {
+  //     var shreya = schools.elements[i];
+  //     var swejal = array.push(shreya);
+  //   }
+  // }
+
+  //console.log(swejal);
+
   return (
-    <Map center={[45.4, -75.7]} zoom={12}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
+    <div className='wrapper'>
+      <div className='dashboard'>
+        <div className='search'>
+          <input type='text' placeholder='Search a school'></input>
+          <i className='fas fa-search'></i>
+        </div>
+        <div className='content'>
+          <li>content</li>
+          <li>content</li>
+          <li>content</li>
+        </div>
+      </div>
+      <div className='container'>
+        <Map center={[27.7201758, 85.3017867]} zoom={10}>
+          <TileLayer
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
 
-      {parkData.features.map(park => (
-        <Marker
-          key={park.properties.PARK_ID}
-          position={[
-            park.geometry.coordinates[1],
-            park.geometry.coordinates[0]
-          ]}
-          onClick={() => {
-            setActivePark(park);
-          }}
-          icon={icon}
-        />
-      ))}
-
-      {activePark && (
+          {Point.map((point) => (
+            <Marker
+              key={point.id}
+              position={[
+                point.geometry.coordinates[1],
+                point.geometry.coordinates[0],
+              ]}
+              onClick={() => {
+                console.log('hy');
+              }}
+              icon={icon}
+            />
+          ))}
+          {/* {activePark && (
         <Popup
           position={[
             activePark.geometry.coordinates[1],
-            activePark.geometry.coordinates[0]
+            activePark.geometry.coordinates[0],
           ]}
           onClose={() => {
             setActivePark(null);
@@ -48,7 +82,9 @@ export default function App() {
             <p>{activePark.properties.DESCRIPTIO}</p>
           </div>
         </Popup>
-      )}
-    </Map>
+      )} */}
+        </Map>
+      </div>
+    </div>
   );
 }
